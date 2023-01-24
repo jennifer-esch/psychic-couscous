fe34a39516905bc2c5b5980ff2da64ad6af5926f
Jennifer Esch <j.esch1@students.uu.nl>
2023-01-24 17:15:24 +0100

    new R script

--- R_script_project.R
+++ R_script_project.R
@@ -4,7 +4,7 @@ people_data <- read_csv('people_data.csv')
 
 people_data_selected_columns <- people_data |>
   select('Name' = 'title',
-         'Description' = 'http://purl.org/dc/elements/1.1/description',
+         'Type' = 'type',
          'Birth_date' = 'ontology/birthDate',
          'Death_date' = 'ontology/deathDate', 
          'Death_place' = 'ontology/deathPlace_label',
