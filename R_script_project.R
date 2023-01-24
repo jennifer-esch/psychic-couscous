d970ab6d004e0f25a470378571a27dbf3d96d80f
Rebecca Fokkink <r.fokkink@students.uu.nl>
2023-01-24 14:23:40 +0100

    Saving data file when filtered ready for analaysis

--- R_script_project.R
+++ R_script_project.R
@@ -1,3 +1,13 @@
 library(tidyverse)
 
-people_data <- read_csv('people_data.csv')
+people_data <- read_csv('psychic-couscous/people_data.csv')
+
+people_data_selected_columns <- people_data |>
+  select('Name' = 'title',
+         'Nationality' = 'ontology/nationality_label',
+         'Description' = 'http://purl.org/dc/elements/1.1/description',
+         'Birth date' = 'ontology/birthDate',
+         'Death date' = 'ontology/deathDate', 
+         'Death place' = 'ontology/deathPlace_label',
+         'Death cause' = 'ontology/deathCause_label'
+         )
