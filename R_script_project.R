9d60dc335cfda4bc894a614b642d30828aee41ff
Rebecca Fokkink <r.fokkink@students.uu.nl>
2023-01-24 16:22:01 +0100

    Saving after categorizing death type

--- R_script_project.R
+++ R_script_project.R
@@ -1,13 +1,17 @@
 library(tidyverse)
 
-people_data <- read_csv('psychic-couscous/people_data.csv')
+people_data <- read_csv('people_data.csv')
 
 people_data_selected_columns <- people_data |>
   select('Name' = 'title',
-         'Nationality' = 'ontology/nationality_label',
          'Description' = 'http://purl.org/dc/elements/1.1/description',
-         'Birth date' = 'ontology/birthDate',
-         'Death date' = 'ontology/deathDate', 
-         'Death place' = 'ontology/deathPlace_label',
-         'Death cause' = 'ontology/deathCause_label'
+         'Birth_date' = 'ontology/birthDate',
+         'Death_date' = 'ontology/deathDate', 
+         'Death_place' = 'ontology/deathPlace_label',
+         'Death_cause' = 'ontology/deathCause_label'
          )
+
+death_cause_mapping <- read_csv('mapping_death_causes.csv')
+
+people_categorized <- left_join(people_data_selected_columns, 
+                                death_cause_mapping, by="Death_cause")
