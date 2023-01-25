91b80409cb169516e9bcf24f99640880ad98affd
Jennifer Esch <j.esch1@students.uu.nl>
2023-01-25 15:50:06 +0100

    Mapping all death causes on R

--- R_script_project.R
+++ R_script_project.R
@@ -10,7 +10,7 @@ people_data_selected_columns <- people_data |>
          'Death_cause' = 'ontology/deathCause_label'
          )
 
-death_cause_mapping <- read_csv('mapping_death_causes.csv')
+death_cause_mapping <- read_csv('mapping_deaths.csv')
 
 people_categorized <- left_join(people_data_selected_columns, 
                                 death_cause_mapping, by="Death_cause")
