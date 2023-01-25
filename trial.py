c987f921880c17c754a4964f1620a7c2cd6b1519
Rebecca Fokkink <r.fokkink@students.uu.nl>
2023-01-25 12:37:54 +0100

    Updated filtering and csv

--- trial.py
+++ trial.py
@@ -1,5 +1,6 @@
 import json
 import pandas as pd
+import csv
 
 #Function to filter every json file for death cause, and whether the person is a politician or activist
 def filter_funct(people_file):
@@ -13,7 +14,7 @@ def filter_funct(people_file):
                 person['ontology/deathCause_label'] = person['ontology/deathCause_label'][0]
             people_filtered.append(person)
 
-    for person in people_filtered:      #loop to filter for politician or activist
+    for person in people_filtered:      #loop to filter for different occupations
         if 'http://purl.org/dc/elements/1.1/description' in person:
             if type(person['http://purl.org/dc/elements/1.1/description']) is list:     #filter if description is a list
                 for description in person['http://purl.org/dc/elements/1.1/description']:
@@ -23,13 +24,13 @@ def filter_funct(people_file):
                     elif 'activist' in description.lower():
                         person['type'] = 'Activist'
                         occupation_filtered.append(person)
-                    elif 'artist' or 'musician' or 'author' or 'actor' or 'writer' in description.lower():
+                    elif ('artist' or 'musician' or 'author' or 'actor' or 'writer') in description.lower():
                         person['type'] = 'Artist'
                         occupation_filtered.append(person)
                     elif 'business' in description.lower():
                         person['type'] = 'Businessperson'
                         occupation_filtered.append(person)
-                    elif 'scholar' or 'professor' or 'academic' in description.lower():
+                    elif ('scholar' or 'professor' or 'academic') in description.lower():
                         person['type'] = 'Scholar'
                         occupation_filtered.append(person)
                     else:
@@ -42,10 +43,10 @@ def filter_funct(people_file):
                 elif 'activist' in person['http://purl.org/dc/elements/1.1/description'].lower():
                     person['type'] = 'Activist'
                     occupation_filtered.append(person)
-                elif 'scholar' or 'professor' or 'academic' in person['http://purl.org/dc/elements/1.1/description'].lower():
+                elif ('scholar' or 'professor' or 'academic') in person['http://purl.org/dc/elements/1.1/description'].lower():
                     person['type'] = 'Scholar'
                     occupation_filtered.append(person)
-                elif 'artist' or 'musician' or 'author' or 'actor' or 'writer' in person['http://purl.org/dc/elements/1.1/description'].lower():
+                elif ('artist' or 'musician' or 'author' or 'actor' or 'writer') in person['http://purl.org/dc/elements/1.1/description'].lower():
                     person['type'] = 'Artist'
                     occupation_filtered.append(person)
                 elif 'business' in person['http://purl.org/dc/elements/1.1/description'].lower():
@@ -61,7 +62,7 @@ alphabet = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q',
 occupation_death_data = []
 for letter in alphabet:
     letter_filtered = filter_funct(f'People/{letter}_people.json')
-    occupation_death_data.append(letter_filtered)
+    occupation_death_data.extend(letter_filtered)
 
 #Create JSON file with selected data
 with open('people_data.json', 'w', encoding = 'utf-8') as file:
@@ -70,6 +71,16 @@ with open('people_data.json', 'w', encoding = 'utf-8') as file:
 with open('people_data.json') as file:
     people_data_json = json.load(file)
 
+with open('people_data.csv', 'w', newline = '', encoding = 'utf-8') as csvfile:
+    fieldnames = ['title','type','ontology/birthDate','ontology/deathDate','ontology/deathCause_label']
+    writer = csv.DictWriter(csvfile, fieldnames=fieldnames, extrasaction= 'ignore')
+    writer.writeheader()
+    for person in people_data_json:
+        writer.writerow(person)
+        
+
+#file.write('Name,Type,Birth_date,Death_date,Death_cause\n')
+
 #Save output into a csv file
-people_data = pd.read_json('people_data.json')
-people_data.to_csv('people_data.csv')
+#people_data = pd.read_json('people_data.json')
+#people_data.to_csv('people_data.csv')
