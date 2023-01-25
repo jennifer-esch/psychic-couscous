bae86f8c315e40b9c7954b4cfbd365d899628f2e
Rebecca Fokkink <r.fokkink@students.uu.nl>
2023-01-25 11:18:49 +0100

    Updated python code with loop for files

--- trial.py
+++ trial.py
@@ -32,43 +32,16 @@ def filter_funct(people_file):
     
     return pol_act_filtered
 
-
-
-
-#Apply the filtering on json file for every letter
-A_people_filt = filter_funct('People/A_people.json')
-B_people_filt = filter_funct('People/B_people.json')
-C_people_filt = filter_funct('People/C_people.json')
-D_people_filt = filter_funct('People/D_people.json')
-E_people_filt = filter_funct('People/E_people.json')
-F_people_filt = filter_funct('People/F_people.json')
-G_people_filt = filter_funct('People/G_people.json')
-H_people_filt = filter_funct('People/H_people.json')
-I_people_filt = filter_funct('People/I_people.json')
-J_people_filt = filter_funct('People/J_people.json')
-K_people_filt = filter_funct('People/K_people.json')
-L_people_filt = filter_funct('People/L_people.json')
-M_people_filt = filter_funct('People/M_people.json')
-N_people_filt = filter_funct('People/N_people.json')
-O_people_filt = filter_funct('People/O_people.json')
-P_people_filt = filter_funct('People/P_people.json')
-Q_people_filt = filter_funct('People/Q_people.json')
-R_people_filt = filter_funct('People/R_people.json')
-S_people_filt = filter_funct('People/S_people.json')
-T_people_filt = filter_funct('People/T_people.json')
-U_people_filt = filter_funct('People/U_people.json')
-V_people_filt = filter_funct('People/V_people.json')
-W_people_filt = filter_funct('People/W_people.json')
-X_people_filt = filter_funct('People/X_people.json')
-Y_people_filt = filter_funct('People/Y_people.json')
-Z_people_filt = filter_funct('People/Z_people.json')
-
-#Merge all seperate lists into a single list
-activist_politician_data = A_people_filt + B_people_filt + C_people_filt + D_people_filt + E_people_filt + F_people_filt + G_people_filt + H_people_filt + I_people_filt + J_people_filt + K_people_filt + L_people_filt + M_people_filt + N_people_filt + O_people_filt + P_people_filt + Q_people_filt + R_people_filt + S_people_filt + T_people_filt + U_people_filt + V_people_filt + W_people_filt + X_people_filt + Y_people_filt + Z_people_filt
+#Apply the filtering on json file for every letter in the alphabet
+alphabet = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+occupation_death_data = []
+for letter in alphabet:
+    letter_filtered = filter_funct(f'People/{letter}_people.json')
+    occupation_death_data.append(letter_filtered)
 
 #Create JSON file with selected data
-with open('people_data.json', 'w', encoding='utf-8') as file:
-    json.dump(activist_politician_data, file, indent=2)
+with open('people_data.json', 'w', encoding = 'utf-8') as file:
+    json.dump(occupation_death_data, file, indent=2)
 
 with open('people_data.json') as file:
     people_data_json = json.load(file)
